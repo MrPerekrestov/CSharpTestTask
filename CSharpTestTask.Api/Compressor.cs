54b0db9208fdf09d267e82b1cb783e1da15959d7
Roman Perekrestov <mr.perekrestov@gmail.com>
2019-11-01 18:31:32 +0100

    going from work on friday

--- CSharpTestTask.Api/Compressor.cs
+++ CSharpTestTask.Api/Compressor.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Threading;
+using System.IO;
 
 namespace CSharpTestTask.Api
 {
     public class Compressor
     {
-        public (string message, bool success) Compress(string fileName)
+        private object key = new object();
+        private int _currentPartNumber;
+        private readonly string _fileName;
+        private readonly int _splitSize;
+
+        private int GetNextPartNumber()
+        {
+            lock (key)
+            {
+                return _currentPartNumber++;
+            }
+        }
+        public Compressor(string fileName)
+        {
+            _fileName = fileName;
+            _splitSize = 1048576;
+        }
+        public Compressor(string fileName, int splitSize)
+        {
+            _fileName = fileName;
+            _splitSize = splitSize;
+        }
+        private void CreatePart()
+        {
+            int readedNumberOfBytes;
+            using (var fileStream = new FileStream(_fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                var buffer = new byte[_splitSize];
+                var partNumber = GetNextPartNumber();
+                var offset = partNumber * _splitSize;
+                fileStream.Seek(offset, SeekOrigin.Begin);
+                Console.WriteLine($"{Thread.CurrentThread.Name}, {partNumber}, {offset}");
+                readedNumberOfBytes = fileStream.Read(buffer, 0, _splitSize);
+                Console.WriteLine($"{Thread.CurrentThread.Name} readed {readedNumberOfBytes} bytes");
+                if (readedNumberOfBytes > 0)
+                {                   
+                    File.WriteAllBytes($"{_fileName}_part{partNumber}.tmp", buffer[0..(readedNumberOfBytes-1)]);
+                }                
+            }
+            if (readedNumberOfBytes == _splitSize)
+            {
+                CreatePart();
+            }
+        }
+        public (string message, bool success) Split()
         {
 
+            var fileSize = new FileInfo(_fileName).Length;
+
+            if (fileSize < _splitSize)
+            {
+                return ("Split is not neccessary, file is too short", true);
+            }
+
+            var numberOfParts = fileSize / _splitSize + 1;
+
+            var numberOfThreadsNeeded =
+                numberOfParts > Environment.ProcessorCount ?
+                Environment.ProcessorCount : numberOfParts;
+
+            for (var i = 0; i < numberOfThreadsNeeded; i++)
+            {
+                var workingThread = new Thread(CreatePart);
+                workingThread.Name = i.ToString();
+                workingThread.Start();
+            }
+            return (numberOfThreadsNeeded.ToString(), true);
         }
     }
 }
--- CSharpTestTask.ConsoleApp/Program.cs
+++ CSharpTestTask.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CSharpTestTask.Api;
+using System;
+using System.IO;
+using System.Threading;
 
 namespace CSharpTestTask.ConsoleApp
 {
@@ -6,7 +9,20 @@ namespace CSharpTestTask.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var compressor = new Compressor("dummy.txt");
+            var result = compressor.Split();
+            //using (var fileStream = new FileStream("dummy.txt", FileMode.Open, FileAccess.Read, FileShare.Read))
+            //{
+            //    var buffer = new byte[1048576];
+            //    var partNumber = 1;
+            //    var offset = partNumber * 1048576;
+            //    fileStream.Seek(offset, SeekOrigin.Begin);
+            //    Console.WriteLine($"{Thread.CurrentThread.Name}, {partNumber}, {offset}");
+            //    var readedNumberOfBytes = fileStream.Read(buffer, 0, 1048576);
+            //    Console.WriteLine($"{Thread.CurrentThread.Name} readed {readedNumberOfBytes} bytes");
+            //    File.WriteAllBytes($"{"dummy.txt"}_part{partNumber}.tmp", buffer);
+            //}
+
         }
     }
 }
